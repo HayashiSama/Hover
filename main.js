75daa27f408d430ce593e6aa61d16ab136af5186
Peter Lin <ptrlin@ucdavis.edu>
2018-01-30 15:48:20 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$("img").hover( 
+	function()
+	{
+		var text = ($(this).attr("src"))
+		$(this).attr("src", $(this).attr("data-alt"));
+		$(this).attr("data-alt", text);
+	},
+	function()
+	{
+		var text = ($(this).attr("src"))
+		$(this).attr("src", $(this).attr("data-alt"));
+		$(this).attr("data-alt", text);
+	}
+
+
+);
